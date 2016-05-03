8b04396b014c47cd8125c14e6d29592b4a4ecfc7
Malcolm Ma <malichaooo@gmail.com>
2016-05-03 18:10:31 -0500

    Add code to process multiple files

--- ReadWrite.cpp
+++ ReadWrite.cpp
@@ -96,12 +96,6 @@ void process(string inputName,string outputName,string ILPOutputName){
 				out<<" + t"<<i+1;
 			out<<";\n";
 
-			//Declare all the variables to be integers
-			out<<"\nint ";
-			for(size_t i=1;i<testCases.size();i++)
-				out<<"t"<<i<<",";
-			out<<"t"<<testCases.size()<<";\n\n";	//Last variable
-
 			//State the constraints,e.g.,1*x1 + 0*x2 + 0*x3 + 0*x4 >=1;
 			for(size_t j=0;j<testCases[0].size();j++){
 				if(testCases[0][j])
@@ -119,6 +113,14 @@ void process(string inputName,string outputName,string ILPOutputName){
 				else
 					out<<" >=0;\n";
 			}
+
+			//Declare all the variables to be integers,otherwise there will be
+			//float type result
+			out<<"\nint ";
+			for(size_t i=1;i<testCases.size();i++)
+				out<<"t"<<i<<",";
+			out<<"t"<<testCases.size()<<";\n\n";	//Last variable
+
 			cout<<"ILP model was saved to \""<<ILPOutputName<<"\"\n";
 		}
 	}catch(exception& e){
@@ -127,13 +129,12 @@ void process(string inputName,string outputName,string ILPOutputName){
 }
 
 int main(int argc,char** argv){
-	if(2==argc){
-		process(argv[1], string(argv[1])+"-greedy", string(argv[1])+"-ilp");
-	}
-	else if (4 == argc)
-		process(argv[1], argv[2], argv[3]);
-	else
+	if(argc<2)
 		throw std::invalid_argument("Argument number mismatched");
+	for(int i=1;i<argc;i++)
+		process(argv[i], "result/"+string(argv[i])+"-greedy", "result/ilp/"+string(argv[i])+"-ilp");
+
+
 
 /*
 	char input[]="Debug/test_case1.txt";
